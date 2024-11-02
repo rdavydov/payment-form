30418c7abc991df30746352ca97972b9c5f9551d
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2024-11-02 14:05:13 +0300

    vite-deploy routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import PaymentForm from "./PaymentForm";
 import RequestDetails from "./RequestDetails";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
+    <>
+      {/* FIXED_CONTENT */}
+
+      <nav>
+        <Link to="/">Payment Form</Link>
+        {" | "}
+        <Link to="/request-details">Request Details</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<PaymentForm />} />
         <Route path="/request-details" element={<RequestDetails />} />
       </Routes>
-    </Router>
+
+      {/* FIXED_CONTENT */}
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PaymentForm from "./PaymentForm";
+import RequestDetails from "./RequestDetails";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <PaymentForm />,
+      },
+      {
+        path: "/request-details",
+        element: <RequestDetails />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
