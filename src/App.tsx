1072e69a11d37ad58813c762548bbf72c8f4ad07
Roman Davydov <15850461+rdavydov@users.noreply.github.com>
2024-11-02 14:16:46 +0300

    vite-react-router fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import { BrowserRouter as Routes, Route, Outlet } from "react-router-dom";
-import PaymentForm from "./PaymentForm";
-import RequestDetails from "./RequestDetails";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<PaymentForm />} />
-        <Route path="/request-details" element={<RequestDetails />} />
-      </Routes>
-
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 };
 
 export default App;
